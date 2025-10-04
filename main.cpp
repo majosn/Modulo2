57764fddc0c1bc41d9e56afda4d12e056b0fe12a
majosn <majo.serrano.notario@gmail.com>
2025-10-03 18:26:27 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,24 @@
-//
-//
-//
+//programa que trabaja con listas ligadas, contiene las funciones de insertar al inicio, al final, eliminar al inicio y al final.
+//A0000000 X, A00000000 X, A01736333 María José Serrano Notario 
+//04/10/25
 
 #include using namespace std;
 
-template
-
-Class Nodo{ T dato; Nodo* next;
-
-    public; T getDato(){ return this ->dato; } void setDato(T dato){ this->dato = dato; } Nodo* getNext(){ return this->next; } void setNext(Nodo* next){ this->next = next; } Nodo(T dato, Nodo* next){ this->dato = dato; this->next = next; }
+template<class T>
+Class Nodo{ 
+    T dato; Nodo* next;
+        public; 
+    T getDato(){ return this ->dato; }
+    void setDato(T dato){ 
+        this->dato = dato;
+    }
+    Nodo* getNext(){ return this->next; } 
+    void setNext(Nodo* next){ this->next = next; } 
+    Nodo(T dato, Nodo* next){ this->dato = dato; this->next = next; }
 
 };
 
 //Función insertar al inicio
-
-template void inserInicio(Nodo** cabeza, T dato) { Nodo* nuevo = new Nodo(dato); nuevo->setNext(*cabeza); *cabeza = nuevo; }
+template void inserInicio(Nodo** cabeza, T dato) { 
+    Nodo* nuevo = new Nodo(dato); nuevo->setNext(*cabeza); *cabeza = nuevo; 
+}
