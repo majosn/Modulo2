593b36ed42e7fe3001f57f7eb9e6e8891a353912
majosn <majo.serrano.notario@gmail.com>
2025-10-03 18:20:18 -0600

    main + insertar inicio

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
 //
-// Created by majo on 10/2/25.
 //
+//
+
+
+#include using namespace std;
+
+template
+
+Class Nodo{ T dato; Nodo* next;
+
+    public; T getDato(){ return this ->dato; } void setDato(T dato){ this->dato = dato; } Nodo* getNext(){ return this->next; } void setNext(Nodo* next){ this->next = next; } Nodo(T dato, Nodo* next){ this->dato = dato; this->next = next; }
+
+};
+
+//Función insertar al inicio
+
+template void inserInicio(Nodo** cabeza, T dato) { Nodo* nuevo = new Nodo(dato); nuevo->setNext(*cabeza); *cabeza = nuevo; }
