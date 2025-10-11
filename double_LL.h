c8c69fedebc53c17426326292406c0d09633d8c6
majosn <majo.serrano.notario@gmail.com>
2025-10-11 11:53:08 -0600

    main en progreso

--- double_LL.h
+++ double_LL.h
@@ -5,6 +5,137 @@
 #ifndef DOUBLE_LL_H
 #define DOUBLE_LL_H
 
+using namespace std;
+#include <iostream>
+
+template <typename T>
+class Nodo {
+private:
+    T dato;
+    Nodo* next;
+    Nodo* prev;
+
+public:
+    // Constructor
+    // O(1)
+    Nodo(T dato) {
+        this->dato = dato;
+        this->next = nullptr;
+        this->prev = nullptr;
+    }
+
+    T getDato() {
+        return dato;
+    }
+
+    void setDato(T dato) {
+        this->dato = dato;
+    }
+
+    Nodo* getNext() {
+        return next;
+    }
+
+    void setNext(Nodo* next) {
+        this->next = next;
+    }
+};
+
+
+//Función insertar al inicio
+//o(1)
+template <typename T>
+void inserta_al_Inicio(Nodo<T>** cabeza, T dato) {
+    Nodo<T>* nuevo = new Nodo<T>(dato);
+
+    if (cabeza == nullptr) {
+        cabeza = nuevo;
+        return;
+    }
+    nuevo->next = cabeza;
+    cabeza->prev = nuevo;
+    cabeza = nuevo;
+}
+
+
+// Funcion inserta al final
+template <typename T>
+void inserta_al_Final(Nodo<T>** cabeza, T dato) {
+    Nodo<T>* nuevo = new Nodo<T>(dato);
+
+    if (cabeza == nullptr) {
+        cabeza = nuevo;
+        return;
+    }
+
+    Nodo<T>* temp = cabeza;
+    while (temp->next != nullptr) {
+        temp = temp->next;
+    }
+
+    temp->next = nuevo;
+    nuevo->prev = temp;
+
+}
+
+
+//Función elimina al inicio
+//o(1)
+template <typename T>
+void elimina_al_Inicio(Nodo<T>** cabeza) {
+
+    if (cabeza == nullptr) {
+        cout << "ERROR" << endl;
+        return;
+    }
+
+    // Update the head pointer and delete the first node.
+    Nodo<T>* temp = cabeza;
+    cabeza = cabeza->next;
+    if (cabeza != nullptr) {
+        cabeza->prev = nullptr;
+    }
+    delete temp;
+}
+
+
+//Función elimina al final
+//o(n)
+template <typename T>
+void elimina_al_Final(Nodo<T>** cabeza) {
+
+    if (cabeza == nullptr) {
+        cout << "ERROR" << endl;
+        return;
+    }
+
+    Nodo<T>* temp = cabeza;
+    if (temp->next == nullptr) {
+        cabeza = nullptr;
+        delete temp;
+        return;
+    }
+
+    while (temp->next != nullptr) {
+        temp = temp->next;
+    }
+
+    temp->prev->next = nullptr;
+    delete temp;
+}
+
+//Imprime
+//o(n)
+template <typename T>
+void imprime(Nodo<T>* cabeza) {
+    Nodo<T>* temp = cabeza;
+    while (temp != nullptr) {
+        cout << temp->data << "\n";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
 
 
 #endif //DOUBLE_LL_H
