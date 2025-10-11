b8e7e21cc520ec8c6e9a89139a9fa5d877b0e3d3
majosn <majo.serrano.notario@gmail.com>
2025-10-11 12:07:01 -0600

    main en progreso

--- main.cpp
+++ main.cpp
@@ -4,7 +4,46 @@
 //15/10/25
 
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
 
 using namespace std;
 
+map<string,int> meses = {
+    {"Jan", 1},
+    {"Feb", 2},
+    {"Mar", 3},
+    {"Apr", 4},
+    {"May", 5},
+    {"Jun", 6},
+    {"Jul", 7},
+    {"Aug", 8},
+    {"Sep", 9},
+    {"Oct", 10},
+    {"Nov", 11},
+    {"Dec", 12},
+};
+
+// Estructura que representa una entrada de la bitácora
+struct Entrada{
+    string mes;
+    int dia;
+    string hora;
+    int ip1;
+    int ip2;
+    int ip3;
+    int ip4;
+    int ipPort;
+    string mensaje;
+};
+
+
+
+
+
+int main() {
+
+
+}
 
