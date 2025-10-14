37a42c209c5e6b2a75427f756f5e1a520bb0377a
mica <rhmixy6@gmail.com>
2025-10-13 20:07:47 -0600

    Create mergeSortDLL.h

---
+++ mergeSortDLL.h
@@ -0,0 +1,29 @@
+// Método de los dos punteros (slow y fast)
+
+template <typename T>
+void splitDLL(Nodo<T>* head, Nodo<T>** left, Nodo<T>** right) {
+    // Si la lista está vacía o solo tiene un nodo
+    if (!head || !head->getNext()) {
+        *left = head;
+        *right = nullptr;
+        return;
+    }
+
+    // Se crean dos punteros
+    Nodo<T>* slow = head; // Avanza de uno en uno
+    Nodo<T>* fast = head; // Avanza de dos en dos
+
+    // Mientras el rápido no llegue al final
+    while (fast->getNext() && fast->getNext()->getNext()) {
+        slow = slow->getNext();             // avanza 1
+        fast = fast->getNext()->getNext();  // avanza 2
+    }
+
+    // slow queda en el nodo del medio
+    *left = head;            // Primera mitad
+    *right = slow->getNext(); // Segunda mitad
+
+    // Se separan las dos listas
+    slow->setNext(nullptr);
+    if (*right) (*right)->setPrev(nullptr);
+}
