919a926f49e09e6c77f95f1089bd3476a45eaeaa
majosn <majo.serrano.notario@gmail.com>
2025-10-15 17:04:43 -0600

    main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//programa que lee bitacora.txt, al,acena los datos en una lista doblemente ligada, ordena por ip o mes o mensaje y despliega los datos en el rango pedido por el usuario,
+//programa que lee bitacora.txt, almacena los datos en una lista doblemente ligada, ordena por ip o mes o mensaje y despliega los datos en el rango pedido por el usuario,
 //devuelve un sorted.txt con los datos ordenados.
 //A01739326 Belsy Aislinn Rueda Santiago, A01739755 Michelle Amanda Hernández Legaria, A01736333 María José Serrano Notario
 //15/10/25
